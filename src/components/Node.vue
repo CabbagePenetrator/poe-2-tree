cdb664c19538d9bddbc2dc658d8dc0841894ecb7
cabbage <cabbagepenetrator@gmail.com>
2024-11-29 21:46:25 +0000

    Draw nodes

---
+++ src/components/Node.vue
@@ -0,0 +1,19 @@
+<script setup>
+const props = defineProps({
+  x: Number,
+  y: Number,
+})
+
+const config = {
+  x: props.x,
+  y: props.y,
+  radius: 15,
+  fill: '#27254e',
+  stroke: '#5d4c2a',
+  strokeWidth: 4,
+}
+</script>
+
+<template>
+  <v-circle :config="config"></v-circle>
+</template>
--- src/components/PassiveSkillTree.vue
+++ src/components/PassiveSkillTree.vue
@@ -1,3 +1,31 @@
+<script setup>
+import Node from '@/components/Node.vue'
+
+const config = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+const nodes = [
+  {
+    id: 1,
+    parent: null,
+    x: 1100,
+    y: 550,
+  },
+  {
+    id: 2,
+    parent: 1,
+    x: 1180,
+    y: 450,
+  },
+]
+</script>
+
 <template>
-  <div>tree</div>
+  <v-stage :config="config">
+    <v-layer>
+      <Node v-for="node in nodes" :x="node.x" :y="node.y" />
+    </v-layer>
+  </v-stage>
 </template>
