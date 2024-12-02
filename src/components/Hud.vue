c7da3e8bf08badb369b1adec40b839e002feda26
Smor <andkarido@gmail.com>
2024-12-02 20:39:52 +0100

    Added static skill points number

---
+++ src/components/Hud.vue
@@ -0,0 +1,11 @@
+<script setup>
+import SkillPoints from '@/components/Hud/SkillPoints.vue'
+
+const props = defineProps({
+  stageSize: Object,
+})
+</script>
+
+<template>
+  <SkillPoints :stageSize="stageSize" />
+</template>
---
+++ src/components/Hud/SkillPoints.vue
@@ -0,0 +1,36 @@
+<script setup>
+const props = defineProps({
+  stageSize: Object,
+})
+
+const skillPointsImage = new Image()
+
+skillPointsImage.src = '/poe2-skill-points.webp'
+</script>
+
+<template>
+  <v-image
+    :config="{
+      x: stageSize.width - skillPointsImage.width/3,
+      y: 0,
+      width: skillPointsImage.width/3,
+      height: skillPointsImage.height/3,
+      image: skillPointsImage,
+    }"
+  />
+
+  <!-- Skill Points Number -->
+  <v-text
+    :config="{
+      x: stageSize.width - 90,
+      y: (skillPointsImage.height/3)/2.5,
+      width: 30,
+      height: (skillPointsImage.height/3)/2.5,
+      text: '123',
+      fill: '#F9E6CA',
+      fontSize: 22,
+      fontFamily: 'Fontin',
+    }"
+  />
+
+</template>
